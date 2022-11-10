0a781c46ba6cd070b8d00d38dc179d2728b6d83f
nbwong <nicholas.b.wong@gmail.com>
2022-11-09 22:21:32 -0800

    updated index.js, resolvers.js, server.js, and auth.js

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,34 +1,40 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        profiles: async () => {
-            return Profile.find();
+        users: async () => {
+            return User.find().populate('books');
         },
-        profile: async (parent, { profileId })=> {
-            return Profile.findOne({_id: profileId });
+        user: async (parent, { username })=> {
+            return User.findOne({ username }).populate('books');
+        },
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+        },
+        book: async (parent, { bookId }) => {
+            return Book.findOne({ _id: bookId });
         },
         me: async (parent, args, context) => {
             if (context.user) {
-                return Profile.findOne({ _id: context.user._id });
+                return User.findOne({ -id: context.user._id }).populate('books');
             }
             throw new AuthenticationError('You need to be logged in!');
         },
     },
     Mutation: {
-        addProfile: async (parent, { name, email, password }) => {
-            const profile = await Profile.create({ name, email, password });
-            const token = signToken(profile);
-
-            return { token, profile };
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
         },
         login: async (parent, {email, password }) => {
-            const profile = await Profile.findOne({ email });
+            const user = await User.findOne({ email });
 
-            if (!profile) {
-                throw new AuthenticationError('No profile with this email found!');
+            if (!user) {
+                throw new AuthenticationError('No user with this email found!');
             }
 
             const correctPw = await profile.isCorrectPassword(password);
@@ -37,12 +43,18 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect password!');
             }
 
-            const token = signToken(profile);
+            const token = signToken(user);
             return { token, profile };
         },
 
-        addSkill: async (parent, { profileId, skill }, context) => {
+        addBook: async (parent, { title }, context) => {
             if (context.user) {
+                const book = await Book.create({
+                    title, 
+                    authors,
+
+                })
+        // NWONG: updating here to add the book to the user's savedBooks
                 return Profile.findOneAndUpdate(
                     { _id: profileId },
                     {
--- server/server.js
+++ server/server.js
@@ -4,14 +4,18 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 // added middleware
 const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
-  
-})
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,6 +27,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+startApolloServer(typeDefs, resolvers);
